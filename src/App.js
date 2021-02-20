7827c050a809861151a8ef2e4f070a6b1d4f9233
Scott Jason <scottleejason@gmail.com>
2021-02-20 08:14:12 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles/main.scss'
+
+const App = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
